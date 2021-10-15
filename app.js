cd8b7ea4d57bed75f58d5555069a6ef4a01b0d6b
zcaldwell <mintyst.john@gmail.com>
2021-10-15 16:19:25 -0700

    this should make the test pass

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-import { foods } from './food.js';
+import { food } from './food.js';
 import { renderFood } from './renderFruits.js';
 
 const foodList = document.getElementById('food-list');
 
-for (let food of foods){
-    const foodListing = renderFood(food);
+for (let foods of food){
+    const foodListing = renderFood(foods);
     foodList.append(foodListing);
 }
